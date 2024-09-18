90302e51bfc907c4a22bc5d4261ab3f785c9122f
MlchaellASC <142234035+MlchaellASC@users.noreply.github.com>
2024-09-18 15:21:54 +0000

    change 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Michael laboy")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("chocolate chip cookie")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
