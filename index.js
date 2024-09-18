8b780d34576fc03c10d9943b1668f72bf04dd480
MlchaellASC <142234035+MlchaellASC@users.noreply.github.com>
2024-09-18 15:21:01 +0000

    change 1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-console.log(Michael laboy)
+console.log("Michael laboy")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-console.log(chocolate chip cookies)
+
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-console.log(Kendrick lamar)
+
 // Stage and commit this change with an appropriate message
 // Go to more.js
